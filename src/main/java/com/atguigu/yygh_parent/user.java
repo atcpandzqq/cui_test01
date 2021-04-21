14c098c79cdb6a9fd2f3d14381bd1c6530de1249
tom_glb <goodMorning_glb@atguigu.com>
2021-04-21 16:09:14 +0800

    本地工程添加到gi上

---
+++ src/main/java/com/atguigu/yygh_parent/user.java
@@ -0,0 +1,13 @@
+package com.atguigu.yygh_parent;
+
+/**
+ * @descriptions:
+ * @author:
+ * @date: 2021/4/21 16:08
+ * @version: 1.0
+ */
+public class user {
+    private int id;
+    private String username;
+    private String password;
+}
